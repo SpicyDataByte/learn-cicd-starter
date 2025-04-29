5bdaab5d79cfa441d685740af4c5d48bf0230fda
SpicyDataByte <matt@hildebrandt.club>
2025-04-29 13:43:35 -0600

    removing unused function

--- main.go
+++ main.go
@@ -24,10 +24,6 @@ type apiConfig struct {
 //go:embed static/*
 var staticFiles embed.FS
 
-func unused() {
-	// this function does nothing
-	// and is called nowhere
-}
 func main() {
 	err := godotenv.Load(".env")
 	if err != nil {
@@ -99,6 +95,5 @@ func main() {
 
 	log.Printf("Serving on port: %s\n", port)
 	log.Fatal(srv.ListenAndServe())
-	
 
 }
